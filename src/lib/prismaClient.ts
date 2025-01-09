ba2c8f1cc81df72bd4e87cba99ce43f5940c656c
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2025-01-09 15:45:06 +0700

    fixing for build

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
 const prisma = new PrismaClient();
 
 export default prisma;
